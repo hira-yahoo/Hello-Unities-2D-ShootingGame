d59fd04d29e20500988a221137844d34f0e19dcd
hira-yahoo <misocasu@yahoo.co.jp>
2015-04-03 11:38:25 +0900

    finished the chapter3.

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	public int speed = 10;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody2D.velocity = transform.up.normalized * speed;
+	}
+	
+}
