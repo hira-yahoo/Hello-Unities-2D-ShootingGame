9da43ce2e176dc54f5de3cd35ee987e45da87016
hira-yahoo <misocasu@yahoo.co.jp>
2015-04-06 10:47:31 +0900

    change the boundary of moving of the player.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -32,8 +32,8 @@ public class Player : MonoBehaviour {
 
 	void Move (Vector2 direction) {
 
-		Vector2 min = Camera.main.ViewportToWorldPoint (new Vector2 (0, 0));
-		Vector2 max = Camera.main.ViewportToWorldPoint (new Vector2 (1, 1));
+		Vector2 min = Camera.main.ViewportToWorldPoint (new Vector2 (0.0f, 0.01f));
+		Vector2 max = Camera.main.ViewportToWorldPoint (new Vector2 (1.0f, 1.0f));
 
 		Vector2 pos = transform.position;
 
