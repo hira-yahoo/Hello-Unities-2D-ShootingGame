e92af1bb3a92b2502ab38cc34bfcde751f3abfb0
hira-yahoo <misocasu@yahoo.co.jp>
2015-04-06 10:25:29 +0900

    add a method as "clamp".

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -29,6 +29,20 @@ public class Player : MonoBehaviour {
 
 		spaceship.Move (direction);
 
+		Clamp ();
+	}
+
+	void Clamp() {
+
+		Vector2 min = Camera.main.ViewportToWorldPoint (new Vector2 (0, 0));
+		Vector2 max = Camera.main.ViewportToWorldPoint (new Vector2 (1, 1));
+
+		Vector2 pos = transform.position;
+
+		pos.x = Mathf.Clamp (pos.x, min.x, max.x);
+		pos.y = Mathf.Clamp (pos.y, min.y, max.y);
+
+		transform.position = pos;
 	}
 
 	public void OnTriggerEnter2D (Collider2D c) {
