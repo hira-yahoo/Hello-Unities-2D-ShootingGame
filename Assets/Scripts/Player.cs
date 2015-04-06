6ee1454a31ee1e07a4ce9b5b5f264d397a05b03f
hira-yahoo <misocasu@yahoo.co.jp>
2015-04-06 10:41:54 +0900

    have the player move by specifying coordinates.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -27,17 +27,17 @@ public class Player : MonoBehaviour {
 
 		Vector2 direction = new Vector2 (x, y).normalized;
 
-		spaceship.Move (direction);
-
-		Clamp ();
+		Move (direction);
 	}
 
-	void Clamp() {
+	void Move (Vector2 direction) {
 
 		Vector2 min = Camera.main.ViewportToWorldPoint (new Vector2 (0, 0));
 		Vector2 max = Camera.main.ViewportToWorldPoint (new Vector2 (1, 1));
 
 		Vector2 pos = transform.position;
+
+		pos += direction * spaceship.speed * Time.deltaTime;
 
 		pos.x = Mathf.Clamp (pos.x, min.x, max.x);
 		pos.y = Mathf.Clamp (pos.y, min.y, max.y);
